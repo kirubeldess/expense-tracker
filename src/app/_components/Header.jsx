252de9d1de679a4852ea170ac095302216e095f1
Kirubel <kirubeldesalegn17@gmail.com>
2025-02-03 13:51:54 +0300

    navbar

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { useUser,UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const Header = () => {
+    const {user, isSignedIn} = useUser();
+  return (
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+      <div className='flex flex-row items-center'>
+        <span className='text-blue-800 font-bold text-xl'>expense-O</span>
+      </div>
+      {isSignedIn ? (<UserButton/>):(
+        <div className='flex gap-3 items-center'>
+            <Link href='/dashboard'>
+                <Button variant='outline' className='rounded-full'>
+                    Dashboard
+                </Button>
+            </Link>
+            <Link href='/dashboard'>
+                <Button className='rounded-full '>
+                    Get Started
+                </Button>
+            </Link>
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default Header
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+// import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "expense-O",
@@ -18,12 +12,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={outfit.className}>
+          {/* <Toaster /> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
